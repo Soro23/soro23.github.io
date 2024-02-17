d19ba8dee900d32304b1925024d356bf184d2a17
Soro23 <aitor23.soro@gmail.com>
2024-02-17 03:38:25 +0100

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Aitor Solana' })
+  res.status(200).json({ name: 'Aitor' })
 }
